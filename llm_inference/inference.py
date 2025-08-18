0e47de3c802378e3958bf150153347fed4ee1ce7
吴子豪 <vortez.wohl@gmail.com>
2025-08-18 14:05:44 +0800

    feat: 设置采样策略默认参数

--- llm_inference/inference.py
+++ llm_inference/inference.py
@@ -2,7 +2,7 @@ from vllm import LLM
 from vllm.sampling_params import BeamSearchParams
 
 
-def inference_with_beam_search(prompt: str, temperature: float, beam_width: int, max_tokens: int, llm: LLM):
+def inference_with_beam_search(prompt: str, llm: LLM, temperature: float = .0, beam_width: int = 8, max_tokens: int = 512):
     beam_search_params = BeamSearchParams(temperature=temperature, beam_width=beam_width, max_tokens=max_tokens)
     results = llm.generate(prompt, beam_search_params)[0]
     return [res.text.strip() for res in results.outputs]
