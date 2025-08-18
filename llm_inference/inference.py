9b5c92c9e14afb93aded143fe672be5c8bdf24b9
吴子豪 <vortez.wohl@gmail.com>
2025-08-18 19:40:45 +0800

    feat: 补充日志

--- llm_inference/inference.py
+++ llm_inference/inference.py
@@ -1,9 +1,14 @@
+import logging
+
 from vllm import LLM, TextPrompt
 from vllm.sampling_params import BeamSearchParams
+
+logger = logging.getLogger('llm_inference')
 
 
 def inference_with_beam_search(prompt: str, llm: LLM, temperature: float = .4, beam_width: int = 5, max_tokens: int = 4096) -> list[tuple[str, float]]:
     beam_search_params = BeamSearchParams(temperature=temperature, beam_width=beam_width, max_tokens=max_tokens, include_stop_str_in_output=False)
     results = llm.beam_search(prompts=[TextPrompt(prompt=prompt)], params=beam_search_params)[0].sequences
     results.sort(key=lambda x: x.cum_logprob, reverse=True)
+    logger.debug(f'inference_with_beam_search: {results}')
     return [(res.text, res.cum_logprob) for res in results]
