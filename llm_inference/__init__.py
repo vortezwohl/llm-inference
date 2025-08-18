5435ffbebd6c847558bc9f51aa0b297c9598255e
吴子豪 <vortez.wohl@gmail.com>
2025-08-18 14:42:36 +0800

    update: 设置默认hf镜像

--- llm_inference/__init__.py
+++ llm_inference/__init__.py
@@ -1,5 +1,5 @@
 import os
 
 __ROOT__ = os.path.dirname(__file__)
-os.environ['TRANSFORMERS_CACHE'] = os.path.join(__ROOT__, '.cache')
+os.environ['HF_HOME'] = os.path.join(__ROOT__, '.cache')
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
