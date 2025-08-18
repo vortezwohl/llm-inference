255e9f1184c44e2fbef48058d4747d5ffe5d1e76
吴子豪 <vortez.wohl@gmail.com>
2025-08-18 19:10:28 +0800

    feat: 实现seed-x模型翻译日志, 并使用 reprompt 策略优化输出稳定性

--- llm_inference/__init__.py
+++ llm_inference/__init__.py
@@ -1,5 +1,16 @@
+import logging
 import os
 
 __ROOT__ = os.path.dirname(__file__)
 os.environ['HF_HOME'] = os.path.join(__ROOT__, '.cache')
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+logger = logging.getLogger('llm_inference')
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(name)s : %(message)s')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -1,12 +1,22 @@
+import logging
+
 from vllm import LLM
 
 from llm_inference.inference import inference_with_beam_search
 
+logger = logging.getLogger('llm_inference')
 seed_x_lm = LLM(model='ByteDance-Seed/Seed-X-PPO-7B-GPTQ-Int8', trust_remote_code=True, gpu_memory_utilization=.8, swap_space=8, cpu_offload_gb=16, max_seq_len_to_capture=2048)
 
 
-def translate(sentence: str, target_lang: str = 'en', **kwargs) -> list[tuple[str, float]]:
+def translate(sentence: str, target_lang: str = 'en', **kwargs) -> str:
     prompt = f'Translate the sentence into <{target_lang.lower().replace("<", "").replace(">", "")}> AFTER explanation in detail: <sentence>{sentence}</sentence> [COT]'
+    logger.debug(f'PROMPT: {prompt.replace("\n", " ")}')
     best_ans = sorted(inference_with_beam_search(prompt=prompt, llm=seed_x_lm, **kwargs), key=lambda x: len(x[0]), reverse=True)
+    logger.debug(f'ANS: {best_ans}')
+    best_ans = best_ans[0]
     prompt = f'{best_ans}<{target_lang.lower().replace("<", "").replace(">", "")}>'
-    return sorted(inference_with_beam_search(prompt=prompt, llm=seed_x_lm, **kwargs), key=lambda x: len(x[0]), reverse=True)
+    logger.debug(f'REPROMPT WITH BEST ANS AFTER BEAM SEARCH: {prompt.replace("\n", " ")}')
+    best_ans = sorted(inference_with_beam_search(prompt=prompt, llm=seed_x_lm, **kwargs), key=lambda x: len(x[0]), reverse=True)
+    logger.debug(f'FIN ANS: {best_ans}')
+    best_ans = best_ans[0]
+    return best_ans[0]
