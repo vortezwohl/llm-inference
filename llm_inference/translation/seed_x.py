03b3f0db3ed0c6a9b32faea059ebe8762145694b
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 16:06:38 +0800

    update

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -29,6 +29,9 @@ def translate(sentence: str, target_lang: str = 'en', resample: int = 1, **kwarg
                            llm=seed_x_lm, **kwargs)[0] if 'en' not in target_lang
                  else 'After all thinking above, the best english translation is:') + '"')
     logger.debug(f'REPROMPT WITH BEST ANS: {prompt.replace("\n", " ")}')
+    kwargs['max_tokens'] = int(len(sentence) * 2.5)
+    if 'zh' in target_lang.lower():
+        kwargs['max_tokens'] = int(len(sentence) * 25)
     kwargs['stop'] = stop_seq
     best_ans = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, regex=regex, **kwargs),
                       key=lambda x: len(x[0]), reverse=True)[0][1:-1].strip()
