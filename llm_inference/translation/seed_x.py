65105646dd73a953dd5140cc87b57f47fcc5a394
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 18:24:53 +0800

    update

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -15,7 +15,7 @@ def translate_cot(sentence: str, target_lang: str = 'en', resample: int = 1, **k
     lang_seq = f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
     kwargs['stop'] = post_think
     prompt = (inference([f'Translate sentence "Translate this sentence into [{target_lang}] and explain in detail '
-                         f'(output_lang={target_lang}, think={True})." into "{target_lang}":{lang_seq}'],
+                         f'(output_lang={target_lang}, explain={True})." into "{target_lang}":{lang_seq}'],
                         llm=seed_x_lm, **kwargs)[0] + f'<sentence>{sentence}</sentence>{lang_seq}{pre_think}')
     logger.debug(f'PROMPT: {prompt.replace("\n", " ")}')
     cot = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, **kwargs),
@@ -29,7 +29,7 @@ def translate(sentence: str, target_lang: str = 'en', **kwargs) -> str:
     stop_seq = f'</{target_lang.lower().replace("<", "").replace(">", "")}>'
     regex = rf'.+"{stop_seq}'
     prompt = inference([f'Translate sentence "Translate this sentence into [{target_lang}] '
-                        f'(output_lang={target_lang}, think={False})." into "{target_lang}":{lang_seq}'],
+                        f'(output_lang={target_lang}, explain={False})." into "{target_lang}" without explain:{lang_seq}'],
                        llm=seed_x_lm, **kwargs)[0] + f'<sentence>{sentence}</sentence>{lang_seq}'
     kwargs['stop'] = stop_seq
     translation = inference(prompt=[prompt], llm=seed_x_lm, regex=regex, **kwargs)[0].strip()
