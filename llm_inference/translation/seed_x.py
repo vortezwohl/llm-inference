7d0e3a13554d7a56ade664a7ad8199e3099e0e0e
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 10:47:24 +0800

    feat: 实现通用采样策略解码

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -2,7 +2,7 @@ import logging
 
 from vllm import LLM
 
-from llm_inference.inference import inference_with_beam_search
+from llm_inference.inference import inference
 
 logger = logging.getLogger('llm_inference')
 seed_x_lm = LLM(model='ByteDance-Seed/Seed-X-PPO-7B-GPTQ-Int8', trust_remote_code=True, gpu_memory_utilization=.975, swap_space=2, cpu_offload_gb=2, max_seq_len_to_capture=4096)
@@ -11,12 +11,12 @@ seed_x_lm = LLM(model='ByteDance-Seed/Seed-X-PPO-7B-GPTQ-Int8', trust_remote_cod
 def translate(sentence: str, target_lang: str = 'en', **kwargs) -> str:
     prompt = f'Translate the sentence into "{target_lang.lower()}" and explain in detail: <sentence>{sentence}</sentence> <{target_lang.lower().replace("<", "").replace(">", "")}>'
     logger.debug(f'PROMPT: {prompt.replace("\n", " ")}')
-    best_ans = sorted(inference_with_beam_search(prompt=prompt, llm=seed_x_lm, **kwargs), key=lambda x: len(x[0]), reverse=True)
+    best_ans = sorted(inference(prompt=prompt, llm=seed_x_lm, **kwargs), key=lambda x: len(x[0]), reverse=True)
     logger.debug(f'ANS: {best_ans}')
     best_ans = best_ans[0]
-    prompt = f'{best_ans}<{target_lang.lower().replace("<", "").replace(">", "")}>'
-    logger.debug(f'REPROMPT WITH BEST ANS AFTER BEAM SEARCH: {prompt.replace("\n", " ")}')
-    best_ans = sorted(inference_with_beam_search(prompt=prompt, llm=seed_x_lm, **kwargs), key=lambda x: len(x[0]), reverse=True)
+    prompt = f'[COT]{best_ans}[COT] Translate the sentence into "{target_lang.lower()}": <sentence>{sentence}</sentence><{target_lang.lower().replace("<", "").replace(">", "")}>'
+    logger.debug(f'REPROMPT WITH BEST ANS: {prompt.replace("\n", " ")}')
+    best_ans = sorted(inference(prompt=prompt, llm=seed_x_lm, **kwargs), key=lambda x: len(x[0]), reverse=True)
     logger.debug(f'FIN ANS: {best_ans}')
     best_ans = best_ans[0]
     return best_ans[0]
