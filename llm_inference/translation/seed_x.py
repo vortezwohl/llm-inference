36c90eb8df99131058bf04ce0df04c3e1a36649e
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 17:50:34 +0800

    update

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -12,13 +12,12 @@ seed_x_lm = LLM(model='ByteDance-Seed/Seed-X-PPO-7B-GPTQ-Int8', trust_remote_cod
 def translate_cot(sentence: str, target_lang: str = 'en', resample: int = 1, **kwargs) -> str:
     pre_think = '[COT]'
     post_think = '[COT]'
-    regex = rf'.+"{post_think}'
     lang_seq = f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
     kwargs['stop'] = post_think
-    prompt = (inference([f'Translate sentence "Translate sentence into [{target_lang}] and explain in detail:" into "{target_lang}":{lang_seq}'],
+    prompt = (inference([f'Translate sentence "Translate sentence into [{target_lang}] and explain in detail." into "{target_lang}":{lang_seq}'],
                         llm=seed_x_lm, **kwargs)[0] + f'<sentence>{sentence}</sentence>{lang_seq}{pre_think}')
     logger.debug(f'PROMPT: {prompt.replace("\n", " ")}')
-    cot = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, regex=regex, **kwargs),
+    cot = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, **kwargs),
                  key=lambda x: len(x[0]), reverse=True)[0]
     logger.debug(f'COT: {cot}')
     return cot
@@ -28,7 +27,7 @@ def translate(sentence: str, target_lang: str = 'en', **kwargs) -> str:
     lang_seq = f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
     stop_seq = '[END]'
     regex = rf'.+"{stop_seq}'
-    prompt = (inference([f'Translate sentence "Translate sentence into [{target_lang}] and explain in detail:" into "{target_lang}":{lang_seq}'],
+    prompt = (inference([f'Translate sentence "Translate sentence into [{target_lang}] without any explain." into "{target_lang}":{lang_seq}'],
                         llm=seed_x_lm, **kwargs)[0] + f'<sentence>{sentence}</sentence>{lang_seq}'
               + lang_seq)
     kwargs['stop'] = stop_seq
