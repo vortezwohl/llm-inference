fdacccaf62382c8ed905d5ec4cc5ac3768fb0864
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 16:19:34 +0800

    update

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -14,7 +14,6 @@ def translate(sentence: str, target_lang: str = 'en', resample: int = 1, **kwarg
     post_think = '[COT]'
     stop_seq = '[END]'
     lang_seq = f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
-    regex = rf'.+"{stop_seq}'
     kwargs['stop'] = post_think
     prompt = (f'Translate sentence "{sentence}" into "{target_lang.lower()}" and explain in detail:'
               f'{lang_seq}{pre_think}')
@@ -26,15 +25,15 @@ def translate(sentence: str, target_lang: str = 'en', resample: int = 1, **kwarg
     prompt = (f'Translate sentence "{sentence}" into "{target_lang.lower()}":'
               f'{lang_seq}{pre_think}{best_ans}{post_think}'
               + (inference([f'Translate sentence "After all thinking above, the best {target_lang} translation is '
-                            f'(only outputs the translated sentence):" into "{target_lang}": {lang_seq}'],
+                            f'(only outputs the translated {target_lang} sentence):" into "{target_lang}": {lang_seq}'],
                            llm=seed_x_lm, **kwargs)[0] if 'en' not in target_lang
                  else 'After all thinking above, the best english translation is '
-                      '(only outputs the translated sentence):')
+                      f'(only outputs the translated {target_lang} sentence):')
               + f'{lang_seq}"')
     logger.debug(f'REPROMPT WITH BEST ANS: {prompt.replace("\n", " ")}')
     kwargs['max_tokens'] = int(len(seed_x_lm.get_tokenizer().encode(sentence)) * 1.75)
     kwargs['stop'] = stop_seq
-    best_ans = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, regex=regex, **kwargs),
+    best_ans = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, **kwargs),
                       key=lambda x: len(x[0]), reverse=True)[0][1:-1].strip()
     logger.debug(f'TRANSLATION: {best_ans}')
     return best_ans
