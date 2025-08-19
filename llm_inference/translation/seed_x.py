982588a7aa563ab8fde343c20fce79c30efd7446
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 14:45:46 +0800

    fix: 修复翻译错误

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -10,19 +10,19 @@ seed_x_lm = LLM(model='ByteDance-Seed/Seed-X-PPO-7B-GPTQ-Int8', trust_remote_cod
 
 
 def translate(sentence: str, target_lang: str = 'en', resample: int = 1, **kwargs) -> str:
+    pre_stop_seq = '<sentence>'
     stop_seq = '</sentence>'
-    regex = rf'.+{stop_seq}'
-    prompt = (f'Translate the sentence into "{target_lang.lower()}" and explain in detail:'
-              f'<sentence>{sentence}{stop_seq}'
+    regex = rf'{pre_stop_seq}.+{stop_seq}'
+    prompt = (f'Translate "{sentence}" into "{target_lang.lower()}" and explain in detail:'
               f'<{target_lang.lower().replace("<", "").replace(">", "")}>[COT]')
     logger.debug(f'PROMPT: {prompt.replace("\n", " ")}')
     best_ans = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, **kwargs),
                       key=lambda x: len(x[0]), reverse=True)
     logger.debug(f'ANS: {best_ans}')
     best_ans = best_ans[0]
-    prompt = (f'[COT]{best_ans}[COT]Translate the sentence into "{target_lang.lower()}" (Give me translation only):'
-              f'<sentence>{sentence}{stop_seq}'
-              f'<{target_lang.lower().replace("<", "").replace(">", "")}><sentence>')
+    prompt = (f'Translate "{sentence}" into "{target_lang.lower()}" and explain in detail:'
+              f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
+              f'[COT]{best_ans}[COT]The final translation is {pre_stop_seq}')
     logger.debug(f'REPROMPT WITH BEST ANS: {prompt.replace("\n", " ")}')
     kwargs['max_tokens'] = int(len(sentence) * 2.5)
     kwargs['stop'] = stop_seq
