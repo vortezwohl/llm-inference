e77ac83be7c12dde50a45b551b88e854ac6a6298
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 17:47:01 +0800

    update

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -15,9 +15,8 @@ def translate_cot(sentence: str, target_lang: str = 'en', resample: int = 1, **k
     regex = rf'.+"{post_think}'
     lang_seq = f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
     kwargs['stop'] = post_think
-    prompt = (inference([f'Translate sentence "Translate sentence [$PLACEHOLDER$] into [{target_lang}] and explain in detail:" into "{target_lang}":{lang_seq}'],
-                        llm=seed_x_lm, **kwargs)[0].replace('[$PLACEHOLDER$]', f'"{sentence}"')
-              + f'{lang_seq}{pre_think}')
+    prompt = (inference([f'Translate sentence "Translate sentence into [{target_lang}] and explain in detail:" into "{target_lang}":{lang_seq}'],
+                        llm=seed_x_lm, **kwargs)[0] + f'<sentence>{sentence}</sentence>{lang_seq}{pre_think}')
     logger.debug(f'PROMPT: {prompt.replace("\n", " ")}')
     cot = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, regex=regex, **kwargs),
                  key=lambda x: len(x[0]), reverse=True)[0]
@@ -29,8 +28,8 @@ def translate(sentence: str, target_lang: str = 'en', **kwargs) -> str:
     lang_seq = f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
     stop_seq = '[END]'
     regex = rf'.+"{stop_seq}'
-    prompt = (inference([f'Translate sentence "Translate sentence [$PLACEHOLDER$] into [{target_lang}] and explain in detail:" into "{target_lang}":{lang_seq}'],
-                        llm=seed_x_lm, **kwargs)[0].replace('[$PLACEHOLDER$]', f'"{sentence}"')
+    prompt = (inference([f'Translate sentence "Translate sentence into [{target_lang}] and explain in detail:" into "{target_lang}":{lang_seq}'],
+                        llm=seed_x_lm, **kwargs)[0] + f'<sentence>{sentence}</sentence>{lang_seq}'
               + lang_seq)
     kwargs['stop'] = stop_seq
     translation = inference(prompt=[prompt], llm=seed_x_lm, regex=regex, **kwargs)[0].strip()
