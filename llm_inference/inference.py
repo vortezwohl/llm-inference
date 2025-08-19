15fccb1f54e525024462d36079c3d4aedfaf7a06
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 10:45:02 +0800

    feat: 实现通用采样策略解码

--- llm_inference/inference.py
+++ llm_inference/inference.py
@@ -1,7 +1,7 @@
 import logging
 
 from vllm import LLM, TextPrompt
-from vllm.sampling_params import BeamSearchParams
+from vllm.sampling_params import BeamSearchParams, SamplingParams
 
 logger = logging.getLogger('llm_inference')
 
@@ -12,3 +12,15 @@ def inference_with_beam_search(prompt: str, llm: LLM, temperature: float = .0, b
     results.sort(key=lambda x: x.cum_logprob, reverse=True)
     logger.debug(f'inference_with_beam_search: {results}')
     return [(res.text, res.cum_logprob) for res in results]
+
+
+def inference(prompt: str, llm: LLM, **kwargs) -> list[str]:
+    max_retry = 8
+    retry = 0
+    while retry < max_retry:
+        results = llm.generate(prompts=[TextPrompt(prompt=prompt)], params=SamplingParams(**kwargs))
+        logger.debug(f'inference: {results}')
+        results = [y.text for y in [x.outputs[0] for x in results] if y.finish_reason.lower() == 'stop']
+        if len(results) > 0:
+            return results
+        retry += 1
