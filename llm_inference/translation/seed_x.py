cc45f94fdf80017066084425d3a2cd4bae3c1110
吴子豪 <vortez.wohl@gmail.com>
2025-08-20 11:16:15 +0800

    feat: 翻译允许指定语境

--- llm_inference/translation/seed_x.py
+++ llm_inference/translation/seed_x.py
@@ -9,12 +9,12 @@ seed_x_lm = LLM(model='ByteDance-Seed/Seed-X-PPO-7B-GPTQ-Int8', trust_remote_cod
                 swap_space=2, cpu_offload_gb=1.5, max_seq_len_to_capture=4096)
 
 
-def translate_cot(sentence: str, target_lang: str = 'en', resample: int = 1, **kwargs) -> str:
+def translate_cot(sentence: str, context: str, target_lang: str = 'en', resample: int = 1, **kwargs) -> str:
     pre_think = '[COT]'
     post_think = '[COT]'
     lang_seq = f'<{target_lang.lower().replace("<", "").replace(">", "")}>'
     kwargs['stop'] = post_think
-    prompt = f'Translate the sentence and explain in detail.<sentence>{sentence}</sentence>{lang_seq}{pre_think}'
+    prompt = f'[CTX]{context}[CTX]Translate the sentence and explain in detail.<sentence>{sentence}</sentence>{lang_seq}{pre_think}'
     logger.debug(f'PROMPT: {prompt.replace("\n", " ")}')
     cot = sorted(inference(prompt=[prompt] * resample, llm=seed_x_lm, **kwargs),
                  key=lambda x: len(x[0]), reverse=True)[0]
